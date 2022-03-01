7d001dcd947af7f0ff76001ecf770caf4785b843
Eli250 <hirwaeli@outlook.com>
2022-03-01 13:16:44 +0200

    Revert "Setting up postgres and sequelize"
    This reverts commit caf872f1d4f0d541c1b25c977cb473d183a61064.

--- src/app.js
+++ src/app.js
@@ -1,24 +1,12 @@
-import express from "express";
-import routes from "routes/index";
-import "dotenv/config";
-const Sequelize = require("sequelize");
+import express from "express"
+import routes from "routes/index"
+import "dotenv/config"
 
-const app = express();
-const port = process.env.PORT || 5000;
-app.use("/api/v1", routes);
 
-//Testing DB Connection
-async function connectToDb() {
-  const sequelize = new Sequelize(process.env.DEV_DATABASE_URL);
-  try {
-    await sequelize.authenticate();
-    console.info("DATABASE CONNECTION SUCCESSFULLY!");
-  } catch (error) {
-    console.error("Unable to connect to the database: ", error);
-  }
-}
-connectToDb();
+const app = express()
+const port = process.env.PORT || 5000
+app.use("/api/v1", routes)
 
 app.listen(port, () => {
-  console.log("server up running on port ", port);
-});
+    console.log("server up running on port ", port)
+})

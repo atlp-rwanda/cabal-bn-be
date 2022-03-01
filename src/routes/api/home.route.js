c35b4d34effa69ffc8a8a0bfec41046844bb6d2b
niyonkurueric <niyonkurufamous@gmail.com>
2022-03-01 19:45:08 +0200

    haund ci

--- src/routes/api/home.route.js
+++ src/routes/api/home.route.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 const home = express.Router();
 
+// eslint-disable-next-line no-unused-vars
 home.get('/', (req, res, next) => {
     res.status(200).send({ message: 'Welcome to our homepage' });
 });

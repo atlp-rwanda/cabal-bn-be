f6cb99c42463c30df662fc40460925e05b62b2b9
friendscareer <86321163+friendscareer@users.noreply.github.com>
2022-03-02 14:25:50 +0200

    #181339468 Setting Database and Sequelize (#7)
    * Setting up postgres and sequelize
    
    * Revert "Setting up postgres and sequelize"
    
    This reverts commit caf872f1d4f0d541c1b25c977cb473d183a61064.
    
    * configure database
    
    * Update .env.sample
    
    Co-authored-by: Eli250 <hirwaeli@outlook.com>
    Co-authored-by: Eli Hirwa <elihirwamugaba@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,12 +1,52 @@
-import express from "express"
-import routes from "routes/index"
-import "dotenv/config"
+import express from "express";
+import routes from "routes/index";
+import "dotenv/config";
+import Sequelize from "sequelize";
 
+const app = express();
+const port = process.env.PORT || 5000;
+const mode = process.env.NODE_ENV || "development";
 
-const app = express()
-const port = process.env.PORT || 5000
-app.use("/api/v1", routes)
+try {
+  //TESTING DATABASE CONNECTION
+  if (mode === "development") {
+    const sequelize = new Sequelize(process.env.DEV_DATABASE_URL);
+    sequelize
+      .authenticate()
+      .then(() => {
+        console.log("DEV DATABASE CONNECTION ESTABLISHED!");
+      })
+      .catch((err) => {
+        console.log("Unable to connect to the database: ", err);
+      });
+  }
+  if (mode === "test") {
+    const sequelize = new Sequelize(process.env.TEST_DATABASE_URL);
+    sequelize
+      .authenticate()
+      .then(() => {
+        console.log("TEST DATABASE CONNECTION ESTABLISHED!");
+      })
+      .catch((err) => {
+        console.log("Unable to connect to the database: ", err);
+      });
+  }
+  if (mode === "production") {
+    const sequelize = new Sequelize(process.env.PROD_DATABASE_URL);
+    sequelize
+      .authenticate()
+      .then(() => {
+        console.log("PRODUCTION DATABASE CONNECTION ESTABLISHED!");
+      })
+      .catch((err) => {
+        console.log("Unable to connect to the database: ", err);
+      });
+  }
+  app.use("/api/v1", routes);
 
-app.listen(port, () => {
-    console.log("server up running on port ", port)
-})
+  app.listen(port, () => {
+    console.log("server up running on port ", port);
+  });
+} catch (error) {
+  console.log(error);
+}

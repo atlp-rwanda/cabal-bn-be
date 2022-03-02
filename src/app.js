c0832249619426ffb874901c56564747e169169b
blandri <landrybrok3@gmail.com>
2022-03-02 20:16:38 +0200

    doing rebase

--- src/app.js
+++ src/app.js
@@ -1,52 +1,53 @@
-import express from "express";
-import routes from "routes/index";
-import "dotenv/config";
-import Sequelize from "sequelize";
+import express from 'express';
+import routes from 'routes/index';
+import 'dotenv/config';
+import Sequelize from 'sequelize';
 
 const app = express();
 const port = process.env.PORT || 5000;
-const mode = process.env.NODE_ENV || "development";
+const mode = process.env.NODE_ENV || 'development';
 
 try {
   //TESTING DATABASE CONNECTION
-  if (mode === "development") {
+  if (mode === 'development') {
     const sequelize = new Sequelize(process.env.DEV_DATABASE_URL);
     sequelize
       .authenticate()
       .then(() => {
-        console.log("DEV DATABASE CONNECTION ESTABLISHED!");
+        console.log('DEV DATABASE CONNECTION ESTABLISHED!');
       })
       .catch((err) => {
-        console.log("Unable to connect to the database: ", err);
+        console.log('Unable to connect to the database: ', err);
       });
   }
-  if (mode === "test") {
+  if (mode === 'test') {
     const sequelize = new Sequelize(process.env.TEST_DATABASE_URL);
     sequelize
       .authenticate()
       .then(() => {
-        console.log("TEST DATABASE CONNECTION ESTABLISHED!");
+        console.log('TEST DATABASE CONNECTION ESTABLISHED!');
       })
       .catch((err) => {
-        console.log("Unable to connect to the database: ", err);
+        console.log('Unable to connect to the database: ', err);
       });
   }
-  if (mode === "production") {
+  if (mode === 'production') {
     const sequelize = new Sequelize(process.env.PROD_DATABASE_URL);
     sequelize
       .authenticate()
       .then(() => {
-        console.log("PRODUCTION DATABASE CONNECTION ESTABLISHED!");
+        console.log('PRODUCTION DATABASE CONNECTION ESTABLISHED!');
       })
       .catch((err) => {
-        console.log("Unable to connect to the database: ", err);
+        console.log('Unable to connect to the database: ', err);
       });
   }
-  app.use("/api/v1", routes);
-
-  app.listen(port, () => {
-    console.log("server up running on port ", port);
-  });
+  app.use('/api/v1', routes);
 } catch (error) {
   console.log(error);
 }
+const server = app.listen(port, () => {
+  console.log('server up running on port ', port);
+});
+
+export default server;

5823c62111e09fc4cf89fb3afc0f4196bfde0423
blandri <landrybrok3@gmail.com>
2022-03-07 12:47:14 +0200

    before rebase

--- src/app.js
+++ src/app.js
@@ -1,36 +1,38 @@
-import express from "express";
-import "dotenv/config";
-import { serve, setup } from "swagger-ui-express";
-import routes from "routes/index";
-import docs from "documentation/index";
-import db from "database/models";
+/* eslint-disable require-jsdoc */
+/* eslint-disable import/no-named-as-default-member */
+import express from 'express';
+import 'dotenv/config';
+import { serve, setup } from 'swagger-ui-express';
+import routes from './routes/index';
+import docs from './documentation/index';
+import db from './database/models';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-console.log("authenticating ....");
+console.log('authenticating ....');
 async function connectdb() {
   try {
     const { sequelize } = db;
     await sequelize.authenticate();
-    console.log("Database established successfully !!");
+    console.log('Database established successfully !!');
   } catch (err) {
-    console.log("failed to connect to the database", err);
+    console.log('failed to connect to the database', err);
   }
 }
 connectdb();
 
-app.use("/api/v1", routes);
-app.use("/api-docs", serve, setup(docs));
+app.use('/api/v1', routes);
+app.use('/api-docs', serve, setup(docs));
 
-app.get("/", function (req, res) {
-  res.status(200).json({message: "Welcome to Barefoot Nomad." });
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to Barefoot Nomad.' });
 });
-app.get("*", function (req, res) {
-  res.status(404).json({message: "Route not found." });
+app.get('*', (req, res) => {
+  res.status(404).json({ message: 'Route not found.' });
 });
 const server = app.listen(port, () => {
-  console.log("server up running on port ", port);
+  console.log('server up running on port ', port);
 });
 
 export default server;

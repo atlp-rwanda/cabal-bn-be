66341451f0385f05038801105a49019b8be9575a
Tsa2341 <alanshema2002@gmail.com>
2022-03-21 13:20:15 +0200

    commit before rebase

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,169 +1,184 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable require-jsdoc */
-import UserService from '../services/user.service';
-const bcrypt = require('bcrypt');
-import { User } from '../database/models';
-import { generateToken, hashPassword, comparePassword } from '../helpers/user.helpers';
 import sgMail from '@sendgrid/mail';
 import { config } from 'dotenv';
-import { confirmEmail } from '../utils/email.utils';
 import jwt from 'jsonwebtoken';
+import UserService from '../services/user.service';
+import { User } from '../database/models';
+import {
+  generateToken,
+  hashPassword,
+  comparePassword
+} from '../helpers/user.helpers';
+import { confirmEmail } from '../utils/email.utils';
+
+const bcrypt = require('bcrypt');
+
 config();
 
-
 export default class UserController {
-    constructor() {
-        this.userService = new UserService();
+  constructor() {
+    this.userService = new UserService();
+  }
+
+  async createUser(req, res) {
+    try {
+      const data = req.body;
+      data.password = hashPassword(data.password);
+      data.role_id = 4;
+      const newUser = await this.userService.createUser(data, res);
+
+      const token = generateToken({ email: newUser.email }, '1d');
+      return res
+        .status(201)
+        .json({ message: 'user registered successfully', token });
+    } catch (error) {
+      return res.status(500).json({
+        message: 'Error occured while creating a user',
+        error: error.message
+      });
     }
+  }
 
-    async createUser(req, res) {
-        try {
-            const data = req.body;
-            data.password = hashPassword(data.password);
-            data.role_id = 4;
-            const newUser = await this.userService.createUser(data, res);
-
-            const token = generateToken({ email: newUser.email }, '1d');
-            return res
-                .status(201)
-                .json({ message: 'user registered successfully', token });
-        } catch (error) {
-            return res.status(500).json({
-                message: 'Error occured while creating a user',
-                error: error.message
-            });
-        }
+  async userLogin(req, res) {
+    try {
+      const user = await this.userService.userLogin(req.body.email, res);
+      const validation = await comparePassword(
+        req.body.password,
+        user.password
+      );
+      if (validation) {
+        const token = await generateToken({ email: user.email }, '1d');
+        return res
+          .status(201)
+          .header('authenticate', token)
+          .json({ message: 'Logged in successfully', token });
+      }
+      console.log(user.password);
+      console.log(req.body.password);
+      return res.status(401).json({ message: 'Invalid password' });
+    } catch (error) {
+      return res.status(404).json({
+        message: 'Error occured while logging in',
+        error
+      });
     }
+  }
 
-    async userLogin(req, res) {
-        try {
-            const user = await this.userService.userLogin(req.body.email, res);
-            const validation = await comparePassword(
-                req.body.password,
-                user.password
-            );
-            if (validation) {
-                const token = await generateToken({ email: user.email }, '1d');
-                return res
-                    .status(201)
-                    .header('authenticate', token)
-                    .json({ message: 'Logged in successfully', token });
-            }
-            console.log(user.password)
-            console.log(req.body.password)
-            return res.status(401).json({ message: 'Invalid password' });
+  static async forgot(req, res) {
+    const exist = await new UserService().userExist(req.body.email);
 
-        } catch (error) {
-            return res.status(404).json({
-                message: 'Error occured while logging in',
-                error
-            });
-        }
+    if (exist.email) {
+      const tokenid = generateToken({ id: exist.id }, '10m');
+      const message = {
+        to: exist.email,
+        from: {
+          name: process.env.EMAIL_NAME,
+          email: process.env.FROM_EMAIL
+        },
+        subject: 'reset password',
+        text: 'request for reset password',
+        html: confirmEmail(tokenid)
+      };
+      sgMail
+        .send(message)
+        .then(() => {
+          res.status(200).json({
+            status: 200,
+            message: 'Password reset link has been sent to your email'
+          });
+        })
+        .catch((error) => console.log(error.message));
+    } else {
+      res.status(404).json({ status: 404, message: 'Email has not found' });
     }
-    static async forgot(req, res) {
+  }
 
-        const exist = await new UserService().userExist(req.body.email);
-
-        if (exist.email) {
-            const tokenid = generateToken({ id: exist.id }, '10m')
-            const message = {
-                to: exist.email,
-                from: {
-                    name: process.env.EMAIL_NAME,
-                    email: process.env.FROM_EMAIL,
-                },
-                subject: 'reset password',
-                text: 'request for reset password',
-                html: confirmEmail(tokenid),
-            }
-            sgMail.send(message)
-                .then(response => {
-                    res.status(200).json({ status: 200, message: "Password reset link has been sent to your email" });
-                })
-                .catch(error => console.log(error.message))
-
-        } else {
-            res.status(404).json({ status: 404, message: 'Email has not found' });
-        }
+  static async reset(req, res) {
+    console.log('runned');
+    try {
+      const { password } = req.body;
+      const { token } = req.params;
+      const userInfo = jwt.verify(token, process.env.SECRETE);
+      const userId = userInfo.id;
+      const newPassword = await bcrypt.hash(password, 10);
+      User.update({ password: newPassword }, { where: { id: userId } });
+      return res
+        .status(200)
+        .send({ message: 'Your new password has been set Return to Login' });
+    } catch (error) {
+      return res.status(500).send({ message: error.message });
     }
-    static async reset(req, res) {
-        try {
-            const { password } = req.body;
-            const { token } = req.params;
-            const userInfo = jwt.verify(token, process.env.SECRETE);
-            const userId = userInfo.id;
-            const newPassword = await bcrypt.hash(password, 10);
-            User.update({ password: newPassword }, { where: { id: userId } })
-            return res.status(200).send({ message: "Your new password has been set Return to Login" });
-        } catch (error) {
-            return res.status(500).send({ message: error.message });
-        }
+  }
 
+  async googleLogin(req, res) {
+    try {
+      const profile = req.user;
+
+      let user = await new UserService().getUser(profile.emails[0].value);
+
+      if (!user) {
+        user = await new UserService().createUser({
+          email: profile.emails && profile.emails[0].value,
+          password: null,
+          role_id: 4,
+          first_name: profile.name && profile.name.familyName,
+          last_name:
+            profile.name &&
+            [profile.name.middleName, profile.name.givenName].join(' '),
+          profile_picture: profile.photos && profile.photos[0].value,
+          provider: 'GOOGLE'
+        });
+      }
+
+      const token = generateToken({ email: user.email, id: user.id }, '1d');
+
+      return res.status(200).json({
+        status: 200,
+        message: 'Logged in successfully',
+        token
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: 'Error occured while logging in',
+        data: error.message
+      });
     }
-    async googleLogin(req, res) {
-        try {
-            const profile = req.user;
+  }
 
-            let user = await new UserService().getUser(profile.emails[0].value);
+  async facebookLogin(req, res) {
+    try {
+      const profile = req.user;
 
-            if (!user) {
-                user = await new UserService().createUser({
-                    email: profile.emails && profile.emails[0].value,
-                    password: null,
-                    role_id: 4,
-                    first_name: profile.name && profile.name.familyName,
-                    last_name: profile.name && [profile.name.middleName, profile.name.givenName].join(' '),
-                    profile_picture: profile.photos && profile.photos[0].value,
-                    provider: 'GOOGLE'
-                });
-            }
+      let user = await new UserService().getUser(profile.emails[0].value);
 
-            const token = generateToken({ email: user.email, id: user.id }, '1d');
+      if (!user) {
+        user = await new UserService().createUser({
+          email: profile.emails && profile.emails[0].value,
+          password: null,
+          role_id: 4,
+          first_name: profile.name && profile.name.familyName,
+          last_name:
+            profile.name &&
+            [profile.name.middleName, profile.name.givenName].join(' '),
+          profile_picture: profile.photos && profile.photos[0].value,
+          provider: 'FACEBOOK'
+        });
+      }
 
-            return res.status(200).json({
-                status: 200,
-                message: 'Logged in successfully',
-                token
-            });
-        } catch (error) {
-            return res.status(500).json({
-                message: 'Error occured while logging in',
-                data: error.message
-            });
-        }
+      const token = generateToken({ email: user.email, id: user.id }, '1d');
+
+      return res.status(200).json({
+        status: 200,
+        message: 'Logged in successfully',
+        token
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: 'Error occured while logging in',
+        data: error.message
+      });
     }
-
-    async facebookLogin(req, res) {
-        try {
-            const profile = req.user;
-
-            let user = await new UserService().getUser(profile.emails[0].value);
-
-            if (!user) {
-                user = await new UserService().createUser({
-                    email: profile.emails && profile.emails[0].value,
-                    password: null,
-                    role_id: 4,
-                    first_name: profile.name && profile.name.familyName,
-                    last_name: profile.name && [profile.name.middleName, profile.name.givenName].join(' '),
-                    profile_picture: profile.photos && profile.photos[0].value,
-                    provider: 'FACEBOOK'
-                });
-            }
-
-            const token = generateToken({ email: user.email, id: user.id }, '1d');
-
-            return res.status(200).json({
-                status: 200,
-                message: 'Logged in successfully',
-                token
-            });
-        } catch (error) {
-            return res.status(500).json({
-                message: 'Error occured while logging in',
-                data: error.message
-            });
-        }
-    }
+  }
 }
